bfddad6c1f108a32c0f99e91a822abea505cd1d4
RedGhoul <avaneesab5@gmail.com>
2019-09-03 09:53:11 -0400

    added uploader

---
+++ AJobBoard/Services/S3Uploader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJobBoard.Services
+{
+    public class S3Uploader
+    {
+    }
+}
