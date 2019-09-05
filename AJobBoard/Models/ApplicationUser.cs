0b65522bae85b736ec4da91da6bb61dad4bcab8a
RedGhoul <avaneesab5@gmail.com>
2019-09-05 12:55:22 -0400

    new models and aws upload class

--- AJobBoard/Models/ApplicationUser.cs
+++ AJobBoard/Models/ApplicationUser.cs
@@ -10,6 +10,8 @@ namespace AJobBoard.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
+        public bool IsJobSeeker { get; set; }
+        public bool IsRecruiter { get; set; }
+        public List<Document> Documents { get; set; }
     }
 }
---
+++ AJobBoard/Models/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AJobBoard.Models
+{
+    public class Document
+    {
+        public Document()
+        {
+            this.DateCreated = DateTime.UtcNow;
+        }
+        public int DocumentId { get; set; }
+        public string URL { get; set; }
+        public bool IsResume { get; set; }
+        public bool IsOtherDoc { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
