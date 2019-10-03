0b6bb98d907a6e116ca4be571af49b66f260943a
RedGhoul <avaneesab5@gmail.com>
2019-10-03 09:49:37 -0400

    added aps

---
+++ Scrapper/JobFinder.py
@@ -0,0 +1,167 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+from datetime import datetime
+from threading import Thread
+import os
+import glob
+
+max_results_per_city = 400
+postionFind = [ "software+developer", "react+developer", 
+                "devops", "software+engineer",
+                "Machine+Learning+Engineer",
+                "Data+Scientist"]
+
+job_Type = "fulltime"
+city_set = ["Ontario", "British+Columbia"]
+max_age = "15"
+host = "ca.indeed.com"
+
+def dotheWork(city, pos, start, finalFileName):
+    df_more = pd.DataFrame(
+        columns=[
+            "Title",
+            "JobURL",
+            "PostDate",
+            "Location",
+            "Company",
+            "Salary",
+            "Synopsis",
+        ]
+    )
+    page = requests.get(
+        "http://"
+        + host
+        + "/jobs?q="
+        + pos
+        + "+%2420%2C000&l="
+        + str(city)
+        + "&jt="
+        + job_Type
+        + "&fromage="
+        + max_age
+        + "&start="
+        + str(start)
+    )
+    time.sleep(1)  # ensuring at least 1 second between page grabs
+    soup = BeautifulSoup(page.text, "html.parser")
+    for each in soup.find_all(class_="result"):
+        try:
+            title = each.find(class_="jobtitle").text.replace("\n", "").replace(",","")
+        except:
+            title = "NULL"
+        #print(title)
+        job_URL = "NULL"
+        try:
+            job_URL = "https://" + host + each.find(class_="jobtitle")["href"].replace(",","")
+            
+        except:
+            job_URL = "NULL"
+        try:
+            if(job_URL is not 'NULL'):
+                mainPage = requests.get(job_URL)
+                time.sleep(1)
+                DescriptionSoup = BeautifulSoup(mainPage.text, "html.parser")
+                synopsis = str(DescriptionSoup.findAll("div", {"id": "jobDescriptionText"})[0].prettify().replace("\n", "").replace(",",""))
+                #print(synopsis)
+                if(synopsis is None):
+                    continue
+        except:
+            synopsis ='NULL'
+
+        try:
+            location = each.find("span", {"class": "location"}).text.replace(
+                "\n", ""
+            ).replace(",","")
+        except:
+            location = "N/A"
+        try:
+            company = each.find(class_="company").text.replace("\n", "").replace(",","")
+        except:
+            company ="NULL"
+        try:
+            salary = each.find(class_="salary.no-wrap").text.replace(",","")
+        except:
+            salary ='N/A'
+
+        try:
+            PostDate = each.find(class_="date").text.replace("\n", "").replace(",","")
+        except:
+            PostDate = "N/A"
+        
+        body = {
+                "Title": title,
+                "JobURL": job_URL,
+                "PostDate": PostDate,
+                "Location": location,
+                "Company": company,
+                "Salary": salary,
+                "Synopsis": synopsis,
+            }
+        #print(body)
+        df_more = df_more.append(
+            body,
+            ignore_index=True,
+        )
+    df_more.to_csv(finalFileName + ".csv", encoding="utf-8",index=False)
+    print(finalFileName + " is done")
+
+def Indeed():
+    workers = []
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S").replace(" ", "").replace(":", "")
+    finalFileName = ("JobPostingIndeed" + dt_string).replace("/", "")
+
+    # scraping code:
+    for city in city_set:
+        for pos in postionFind:
+            for start in range(0, max_results_per_city, 10):
+                thread1 = Thread(
+                    target=dotheWork,
+                    args=(city, pos, start, city + pos + str(start) + finalFileName),
+                )
+                workers.append(thread1)
+
+    for process in workers:
+        process.start()
+
+    for process in workers:
+        process.join()
+
+
+if __name__ == "__main__":
+    start = time.time()
+
+    # print("Starting Download")
+    # Indeed()
+    # print("Completed Download")
+    extension = 'csv'
+    dirr = os.getcwd()
+    print(dirr)
+    os.chdir(dirr)
+    result = glob.glob('*.{}'.format(extension))
+    all_filenames = [i for i in glob.glob("*.{}".format(extension))]
+    # combine all files in the list
+    rays = []
+    for x in all_filenames:
+        newdata = pd.read_csv(x)
+        rays.append(newdata)
+
+    print("Number of part files: " + str(len(rays)))
+
+    df_more = pd.concat(rays)
+    df_more.drop_duplicates(subset= 'Synopsis',inplace = True) 
+
+    df_more.reset_index(drop=True)
+    dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S").replace(" ", "").replace(":", "")
+    finalFileName = ("IndeedJobDump" + dt_string).replace("/", "")
+    df_more.to_csv(finalFileName+".csv", encoding="utf-8",index=False)
+
+    print("Doing Clean Up")
+    for x in all_filenames:
+        os.remove(x)
+
+    # end = time.time()
+    # print(end - start)
--- Scrapper/index.py
+++ Scrapper/index.py
@@ -1,167 +1,29 @@
-import requests
-import bs4
-from bs4 import BeautifulSoup
-import pandas as pd
-import time
-from datetime import datetime
-from threading import Thread
-import os
-import glob
+from pytz import utc
 
-max_results_per_city = 400
-postionFind = [ "software+developer", "react+developer", 
-                "devops", "software+engineer",
-                "Machine+Learning+Engineer",
-                "Data+Scientist"]
-
-job_Type = "fulltime"
-city_set = ["Ontario", "British+Columbia"]
-max_age = "15"
-host = "ca.indeed.com"
-
-def dotheWork(city, pos, start, finalFileName):
-    df_more = pd.DataFrame(
-        columns=[
-            "Title",
-            "JobURL",
-            "PostDate",
-            "Location",
-            "Company",
-            "Salary",
-            "Synopsis",
-        ]
-    )
-    page = requests.get(
-        "http://"
-        + host
-        + "/jobs?q="
-        + pos
-        + "+%2420%2C000&l="
-        + str(city)
-        + "&jt="
-        + job_Type
-        + "&fromage="
-        + max_age
-        + "&start="
-        + str(start)
-    )
-    time.sleep(1)  # ensuring at least 1 second between page grabs
-    soup = BeautifulSoup(page.text, "html.parser")
-    for each in soup.find_all(class_="result"):
-        try:
-            title = each.find(class_="jobtitle").text.replace("\n", "").replace(",","")
-        except:
-            title = "NULL"
-        #print(title)
-        job_URL = "NULL"
-        try:
-            job_URL = "https://" + host + each.find(class_="jobtitle")["href"].replace(",","")
-            
-        except:
-            job_URL = "NULL"
-        try:
-            if(job_URL is not 'NULL'):
-                mainPage = requests.get(job_URL)
-                time.sleep(1)
-                DescriptionSoup = BeautifulSoup(mainPage.text, "html.parser")
-                synopsis = str(DescriptionSoup.findAll("div", {"id": "jobDescriptionText"})[0].prettify().replace("\n", "").replace(",",""))
-                #print(synopsis)
-                if(synopsis is None):
-                    continue
-        except:
-            synopsis ='NULL'
-
-        try:
-            location = each.find("span", {"class": "location"}).text.replace(
-                "\n", ""
-            ).replace(",","")
-        except:
-            location = "N/A"
-        try:
-            company = each.find(class_="company").text.replace("\n", "").replace(",","")
-        except:
-            company ="NULL"
-        try:
-            salary = each.find(class_="salary.no-wrap").text.replace(",","")
-        except:
-            salary ='N/A'
-
-        try:
-            PostDate = each.find(class_="date").text.replace("\n", "").replace(",","")
-        except:
-            PostDate = "N/A"
-        
-        body = {
-                "Title": title,
-                "JobURL": job_URL,
-                "PostDate": PostDate,
-                "Location": location,
-                "Company": company,
-                "Salary": salary,
-                "Synopsis": synopsis,
-            }
-        #print(body)
-        df_more = df_more.append(
-            body,
-            ignore_index=True,
-        )
-    df_more.to_csv(finalFileName + ".csv", encoding="utf-8",index=False)
-    print(finalFileName + " is done")
-
-def Indeed():
-    workers = []
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S").replace(" ", "").replace(":", "")
-    finalFileName = ("JobPostingIndeed" + dt_string).replace("/", "")
-
-    # scraping code:
-    for city in city_set:
-        for pos in postionFind:
-            for start in range(0, max_results_per_city, 10):
-                thread1 = Thread(
-                    target=dotheWork,
-                    args=(city, pos, start, city + pos + str(start) + finalFileName),
-                )
-                workers.append(thread1)
-
-    for process in workers:
-        process.start()
-
-    for process in workers:
-        process.join()
+from apscheduler.schedulers.background import BlockingScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
 
 
-if __name__ == "__main__":
-    start = time.time()
+jobstores = {
+   'default': SQLAlchemyJobStore(url="mysql://APSchedulerJobs:YauaX6pIo9v37w6B@157.245.6.60:3306/APSchedulerJobs?ssl=true",tablename='APSchedulerJobs')
+}
+executors = {
+    'default': ThreadPoolExecutor(20),
+    'processpool': ProcessPoolExecutor(5)
+}
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 3
+}
+scheduler = BlockingScheduler(jobstores=jobstores, executors=executors, job_defaults=job_defaults, timezone=utc)
 
-    # print("Starting Download")
-    # Indeed()
-    # print("Completed Download")
-    extension = 'csv'
-    dirr = os.getcwd()
-    print(dirr)
-    os.chdir(dirr)
-    result = glob.glob('*.{}'.format(extension))
-    all_filenames = [i for i in glob.glob("*.{}".format(extension))]
-    # combine all files in the list
-    rays = []
-    for x in all_filenames:
-        newdata = pd.read_csv(x)
-        rays.append(newdata)
+def timed_job():
+    print('This job is run every three minutes.')
 
-    print("Number of part files: " + str(len(rays)))
+job = scheduler.add_job(timed_job, 'interval', minutes=2)
 
-    df_more = pd.concat(rays)
-    df_more.drop_duplicates(subset= 'Synopsis',inplace = True) 
 
-    df_more.reset_index(drop=True)
-    dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S").replace(" ", "").replace(":", "")
-    finalFileName = ("IndeedJobDump" + dt_string).replace("/", "")
-    df_more.to_csv(finalFileName+".csv", encoding="utf-8",index=False)
 
-    print("Doing Clean Up")
-    for x in all_filenames:
-        os.remove(x)
 
-    # end = time.time()
-    # print(end - start)
+scheduler.start()
