d035e77857c945c74cfd9b1f2e4568703e855bb1
RedGhoul <avaneesab5@gmail.com>
2019-10-03 20:00:02 -0400

    100 to 20

--- Scrapper/JobFinder.py
+++ Scrapper/JobFinder.py
@@ -7,13 +7,17 @@ from threading import Thread
 import json
 import secret
 
-max_results_per_city = 100
-postionFind = [ "software+developer", "react+developer", 
-                "devops", "software+engineer",
-                "Machine+Learning+Engineer",
-                "Data+Scientist",
-                "junior+software+developer",
-                "senior+software+developer"]
+max_results_per_city = 20
+postionFind = [
+    "software+developer",
+    "react+developer",
+    "devops",
+    "software+engineer",
+    "Machine+Learning+Engineer",
+    "Data+Scientist",
+    "junior+software+developer",
+    "senior+software+developer",
+]
 
 job_Type = "fulltime"
 city_set = ["Ontario", "British+Columbia"]
@@ -25,17 +29,21 @@ techTrans = "https://techtransparency93.azurewebsites.net/api/JobPostingsAPI/"
 techTransCheck = "https://techtransparency93.azurewebsites.net/api/JobPostingsAPI/Check"
 
 
+header = {
+    "Content-type": "application/json",
+    "Accept": "text/plain",
+    "auth": secret.getAuthKey(),
+}
 
-header = {"Content-type": "application/json",
-          "Accept": "text/plain", "auth":secret.getAuthKey()} 
 
 def checkifdup(urlIn):
     r = json.dumps({"url": urlIn})
-    response = requests.post(url =techTransCheck, data = r,headers=header)
+    response = requests.post(url=techTransCheck, data=r, headers=header)
     time.sleep(1)
     print(response.content)
     result = json.loads(response.content)
     return result
+
 
 def dotheWork(city, pos, start, finalFileName):
     page = requests.get(
@@ -56,70 +64,82 @@ def dotheWork(city, pos, start, finalFileName):
     soup = BeautifulSoup(page.text, "html.parser")
     for each in soup.find_all(class_="result"):
         try:
-            title = each.find(class_="jobtitle").text.replace("\n", "").replace(",","")
+            title = each.find(class_="jobtitle").text.replace("\n", "").replace(",", "")
         except:
             title = "NULL"
-        #print(title)
+        # print(title)
         job_URL = "NULL"
         try:
-            job_URL = "https://" + host + each.find(class_="jobtitle")["href"].replace(",","")
-            
+            job_URL = (
+                "https://"
+                + host
+                + each.find(class_="jobtitle")["href"].replace(",", "")
+            )
+
         except:
             job_URL = "NULL"
         try:
-            if(job_URL is not 'NULL'):
+            if job_URL is not "NULL":
                 mainPage = requests.get(job_URL)
                 time.sleep(1)
                 DescriptionSoup = BeautifulSoup(mainPage.text, "html.parser")
-                synopsis = str(DescriptionSoup.findAll("div", {"id": "jobDescriptionText"})[0].prettify().replace("\n", "").replace(",",""))
-                #print(synopsis)
-                if(synopsis is None):
+                synopsis = str(
+                    DescriptionSoup.findAll("div", {"id": "jobDescriptionText"})[0]
+                    .prettify()
+                    .replace("\n", "")
+                    .replace(",", "")
+                )
+                # print(synopsis)
+                if synopsis is None:
                     continue
         except:
-            synopsis ='NULL'
+            synopsis = "NULL"
 
         try:
-            location = each.find("span", {"class": "location"}).text.replace(
-                "\n", ""
-            ).replace(",","")
+            location = (
+                each.find("span", {"class": "location"})
+                .text.replace("\n", "")
+                .replace(",", "")
+            )
         except:
             location = "N/A"
         try:
-            company = each.find(class_="company").text.replace("\n", "").replace(",","")
+            company = (
+                each.find(class_="company").text.replace("\n", "").replace(",", "")
+            )
         except:
-            company ="NULL"
+            company = "NULL"
         try:
-            salary = each.find(class_="salary.no-wrap").text.replace(",","")
+            salary = each.find(class_="salary.no-wrap").text.replace(",", "")
         except:
-            salary ='N/A'
+            salary = "N/A"
 
         try:
-            PostDate = each.find(class_="date").text.replace("\n", "").replace(",","")
+            PostDate = each.find(class_="date").text.replace("\n", "").replace(",", "")
         except:
             PostDate = "N/A"
-        if job_URL != 'NULL':
+        if job_URL != "NULL":
             if checkifdup(job_URL) == True:
                 print("Found new URL")
                 body = {
-                        "title": title,
-                        "url": job_URL,
-                        "postDate": PostDate,
-                        "location": location,
-                        "company": company,
-                        "salary": salary,
-                        "summary": synopsis,
-                        "numberOfApplies": 0,
-                        "numberOfViews": 0,
-                        "poster": None,
-                        "posters":"ddd",
-                        "jobSource":"Indeed"
-                    }
+                    "title": title,
+                    "url": job_URL,
+                    "postDate": PostDate,
+                    "location": location,
+                    "company": company,
+                    "salary": salary,
+                    "summary": synopsis,
+                    "numberOfApplies": 0,
+                    "numberOfViews": 0,
+                    "poster": None,
+                    "posters": "ddd",
+                    "jobSource": "Indeed",
+                }
                 r = json.dumps(body)
-                mainPage = requests.post(url =techTrans, data = r,headers=header)
+                mainPage = requests.post(url=techTrans, data=r, headers=header)
             else:
                 print("Already found this URL")
 
-    
 
 def Indeed():
     workers = []
@@ -150,6 +170,6 @@ def startup():
     print("Starting Download")
     Indeed()
     print("Completed Download")
-   
+
     end = time.time()
     print(end - start)
