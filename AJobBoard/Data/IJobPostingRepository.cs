fc328e1c40f97dc17e7480a1bec01b0772a58a54
RedGhoul <avaneesab5@gmail.com>
2019-12-08 10:16:15 -0500

    now using JobPostingExistsByDescription

--- AJobBoard/Data/IJobPostingRepository.cs
+++ AJobBoard/Data/IJobPostingRepository.cs
@@ -10,6 +10,7 @@ namespace AJobBoard.Data
         Task<JobPosting> GetJobPostingById(int id);
         Task<IEnumerable<JobPosting>> GetJobPostingsAsync(int amount);
         Task<bool> JobPostingExistsByUrl(string url);
+        Task<bool> JobPostingExistsByDescription(string Summary);
         Task<JobPosting> PutJobPostingAsync(int id, JobPosting jobPosting);
         Task<JobPosting> CreateJobPostingAsync(JobPosting jobPosting);
         Task<JobPosting> DeleteJobPostingAsync(int id);
--- AJobBoard/Models/DTO/TestCheckDTO.cs
+++ AJobBoard/Models/DTO/TestCheckDTO.cs
@@ -8,5 +8,6 @@ namespace AJobBoard.Models.DTO
     public class TestCheckDTO
     {
         public string url { get; set; }
+        public string description { get; set; }
     }
 }
