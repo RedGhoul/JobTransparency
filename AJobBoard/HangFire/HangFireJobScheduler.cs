d6595f1a6741eef03ee4d3349f9e517da62f194a
RedGhoul <avaneesab5@gmail.com>
2020-11-06 21:54:44 -0500

    yolo

--- AJobBoard/HangFire/HangFireJobScheduler.cs
+++ AJobBoard/HangFire/HangFireJobScheduler.cs
@@ -11,17 +11,17 @@ namespace Jobtransparency.Utils.HangFire
             RecurringJob.RemoveIfExists(nameof(KeyPhraseGeneratorJob));
             RecurringJob.AddOrUpdate<KeyPhraseGeneratorJob>(nameof(KeyPhraseGeneratorJob),
                 job => job.Run(JobCancellationToken.Null),
-                Cron.Weekly(DayOfWeek.Monday, 5, 33), TimeZoneInfo.Local);
+                Cron.Weekly(DayOfWeek.Monday, 5, 33), TimeZoneInfo.Local, "KeyPhraseGeneratorJob");
 
             RecurringJob.RemoveIfExists(nameof(SummaryGeneratorJob));
             RecurringJob.AddOrUpdate<SummaryGeneratorJob>(nameof(SummaryGeneratorJob),
                 job => job.Run(JobCancellationToken.Null),
-                Cron.Weekly(DayOfWeek.Friday, 5, 33), TimeZoneInfo.Local);
+                Cron.Weekly(DayOfWeek.Friday, 5, 33), TimeZoneInfo.Local, "SummaryGeneratorJob");
 
             RecurringJob.RemoveIfExists(nameof(ReIndexJobPostingsJob));
             RecurringJob.AddOrUpdate<ReIndexJobPostingsJob>(nameof(ReIndexJobPostingsJob),
                 job => job.Run(JobCancellationToken.Null),
-                Cron.Monthly(27, 1), TimeZoneInfo.Local);
+                Cron.Monthly(27, 1), TimeZoneInfo.Local, "ReIndexJobPostingsJob");
         }
     }
 }
