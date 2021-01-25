50985c9ec4ce0e63c1398905a7f92fbcc4f939a1
RedGhoul <avaneesab5@gmail.com>
2021-01-24 21:50:28 -0500

    Added Process count

--- AJobBoard/ConfigureAppExtentions.cs
+++ AJobBoard/ConfigureAppExtentions.cs
@@ -45,7 +45,7 @@ namespace Jobtransparency
                 SchedulePollingInterval = TimeSpan.FromMinutes(1),
                 HeartbeatInterval = TimeSpan.FromSeconds(20),
                 ServerCheckInterval = TimeSpan.FromSeconds(20),
-                WorkerCount = 4,
+                WorkerCount = Environment.ProcessorCount,
                 ServerName = "Jobs"
             });
             app.UseHangfireDashboard("/hangfire", new DashboardOptions
