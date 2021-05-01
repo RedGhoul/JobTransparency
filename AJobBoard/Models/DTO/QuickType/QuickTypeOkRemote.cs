b72ad0c3714fca118c97d6ab4917d59eb2174a44
RedGhoul <avaneesab5@gmail.com>
2021-05-01 09:33:34 -0400

    Started integrating in OkRemote API

---
+++ AJobBoard/Models/DTO/QuickType/QuickTypeOkRemote.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jobtransparency.Models.DTO.QuickType
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class OkRemoteData
+    {
+        [JsonProperty("legal", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string Legal { get; set; }
+
+        [JsonProperty("explanation", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string Explanation { get; set; }
+
+        [JsonProperty("slug", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string Slug { get; set; }
+
+        [JsonProperty("id", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long? Id { get; set; }
+
+        [JsonProperty("epoch", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long? Epoch { get; set; }
+
+        [JsonProperty("date", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public DateTimeOffset? Date { get; set; }
+
+        [JsonProperty("company", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string Company { get; set; }
+
+        [JsonProperty("company_logo", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string CompanyLogo { get; set; }
+
+        [JsonProperty("position", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string Position { get; set; }
+
+        [JsonProperty("tags", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public List<string> Tags { get; set; }
+
+        [JsonProperty("logo", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public Uri Logo { get; set; }
+
+        [JsonProperty("description", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string Description { get; set; }
+
+        [JsonProperty("location", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public string Location { get; set; }
+
+        [JsonProperty("original", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public bool? Original { get; set; }
+
+        [JsonProperty("url", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public Uri Url { get; set; }
+
+        [JsonProperty("apply_url", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
+        public Uri ApplyUrl { get; set; }
+    }
+
+    public partial class OkRemoteData
+    {
+        public static List<OkRemoteData> FromJson(string json) => JsonConvert.DeserializeObject<List<OkRemoteData>>(json, Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this List<OkRemoteData> self) => JsonConvert.SerializeObject(self, Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+}
