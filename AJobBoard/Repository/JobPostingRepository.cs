96e90b62a26952f9515dcc47a2591313936edc6f
RedGhoul <avaneesab5@gmail.com>
2021-06-05 12:15:02 -0400

    Updated search

--- AJobBoard/Repository/JobPostingRepository.cs
+++ AJobBoard/Repository/JobPostingRepository.cs
@@ -212,19 +212,9 @@ namespace AJobBoard.Data
             }
             List<JobPosting> resultSet;
 
-            if (string.IsNullOrEmpty(homeIndexVm.FindModel.KeyWords))
-            {
-                resultSet = await _ctx.JobPostings.Include(x => x.KeyPhrases).Include(x => x.Tags)
-                   .OrderByDescending(x => x.DateAdded).Skip(fromNumber).Take(12).ToListAsync();
-            }
-            else
-            {
-                var sql = $@"SELECT * FROM [JobPostings] WHERE 
-                    FREETEXT ((Summary,Title,Company,Location), N'{homeIndexVm.FindModel.KeyWords}')";
-                resultSet = await _ctx.JobPostings.FromSqlRaw(sql).Include(x => x.KeyPhrases)
-                    .Include(x => x.Tags)
-                    .OrderByDescending(x => x.DateAdded).Skip(fromNumber).Take(12).ToListAsync();
-            }
+            resultSet = await _ctx.JobPostings.Include(x => x.KeyPhrases).Include(x => x.Tags)
+                  .Where(x => x.SearchVector.Matches(homeIndexVm.FindModel.KeyWords))
+                  .OrderByDescending(x => x.DateAdded).Skip(fromNumber).Take(12).ToListAsync();
 
 
             return _mapper.Map<List<JobPostingDTO>>(resultSet);
