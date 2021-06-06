d6d5e9788b5feb4502c353355b30a1837aedf049
RedGhoul <avaneesab5@gmail.com>
2021-06-06 15:42:11 -0400

    Added new job to HangFireJobScheduler

--- AJobBoard/HangFire/HangFireJobScheduler.cs
+++ AJobBoard/HangFire/HangFireJobScheduler.cs
@@ -12,42 +12,47 @@ namespace Jobtransparency.Utils.HangFire
             RecurringJob.RemoveIfExists(nameof(RemotiveIoDataJob));
             RecurringJob.AddOrUpdate<RemotiveIoDataJob>(nameof(RemotiveIoDataJob),
                 job => job.Run(JobCancellationToken.Null),
-                Cron.Daily(1, 12), TimeZoneInfo.Local);
+                Cron.Daily(1, 12), TimeZoneInfo.Utc);
+
+            RecurringJob.RemoveIfExists(nameof(GenerateKeyTopKeyWordsJobs));
+            RecurringJob.AddOrUpdate<GenerateKeyTopKeyWordsJobs>(nameof(GenerateKeyTopKeyWordsJobs),
+                job => job.Run(JobCancellationToken.Null),
+                Cron.Daily(1, 20), TimeZoneInfo.Utc);
 
             RecurringJob.RemoveIfExists(nameof(OkRemoteJobByTags));
             RecurringJob.AddOrUpdate<OkRemoteJobByTags>(nameof(OkRemoteJobByTags),
                 job => job.Run(JobCancellationToken.Null),
-                Cron.Daily(1, 10), TimeZoneInfo.Local);
+                Cron.Daily(1, 10), TimeZoneInfo.Utc);
 
             RecurringJob.RemoveIfExists(nameof(OkRemoteJob));
             RecurringJob.AddOrUpdate<OkRemoteJob>(nameof(OkRemoteJob),
                 job => job.Run(JobCancellationToken.Null),
-                "0 */2 * * *", TimeZoneInfo.Local);
+                "0 */2 * * *", TimeZoneInfo.Utc);
 
             RecurringJob.RemoveIfExists(nameof(GetJobPostingsJob));
             RecurringJob.AddOrUpdate<GetJobPostingsJob>(nameof(GetJobPostingsJob),
                 job => job.Run(JobCancellationToken.Null),
-                Cron.Daily(1, 20), TimeZoneInfo.Local);
+                Cron.Daily(1, 20), TimeZoneInfo.Utc);
 
             RecurringJob.RemoveIfExists(nameof(KeyPhraseGeneratorJob));
             RecurringJob.AddOrUpdate<KeyPhraseGeneratorJob>(nameof(KeyPhraseGeneratorJob),
                 job => job.Run(JobCancellationToken.Null),
-                Cron.Daily(2, 20), TimeZoneInfo.Local);
+                Cron.Daily(2, 20), TimeZoneInfo.Utc);
 
             RecurringJob.RemoveIfExists(nameof(SummaryGeneratorJob));
             RecurringJob.AddOrUpdate<SummaryGeneratorJob>(nameof(SummaryGeneratorJob),
                 job => job.Run(JobCancellationToken.Null),
-                Cron.Daily(3, 20), TimeZoneInfo.Local);
+                Cron.Daily(3, 20), TimeZoneInfo.Utc);
 
             RecurringJob.RemoveIfExists(nameof(SentimentGeneratorJob));
             RecurringJob.AddOrUpdate<SentimentGeneratorJob>(nameof(SentimentGeneratorJob),
                 job => job.Run(JobCancellationToken.Null),
-                Cron.Daily(4, 20), TimeZoneInfo.Local);
+                Cron.Daily(4, 20), TimeZoneInfo.Utc);
 
             RecurringJob.RemoveIfExists(nameof(IsJobExpiredJobPostingsJob));
             RecurringJob.AddOrUpdate<IsJobExpiredJobPostingsJob>(nameof(IsJobExpiredJobPostingsJob),
                 job => job.Run(JobCancellationToken.Null),
-                Cron.Weekly(DayOfWeek.Monday, 1, 1), TimeZoneInfo.Local);
+                Cron.Weekly(DayOfWeek.Monday, 1, 1), TimeZoneInfo.Utc);
         }
     }
 }
